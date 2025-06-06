67b090088433ea58a9451380f2402efdf9e87a88
Angel8118 <angel8118campos@gmail.com>
2025-06-06 12:40:08 -0600

    installing headlessUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useMemo } from "react";
 import BudgetForm from "./components/BudgetForm";
 import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
+import ExpenseModal from "./components/ExpenseModal";
 
 export default function App() {
 
   const { state } = useBudget()
 
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
-  
+
   return (
     <>
       <header className=" bg-blue-600 py-8 max-h-72">
@@ -18,8 +19,15 @@ export default function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        {isValidBudget ? <BudgetTracker/> : <BudgetForm/>}
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
+
     </>
   )
 }
