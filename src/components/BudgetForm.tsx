9082fc1784633f13d3df6c85d426e4216af89850
Angel8118 <angel8118campos@gmail.com>
2025-06-05 22:47:50 -0600

    component: created component budgetform.

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,47 @@
+import { useMemo, useState, type ChangeEvent } from "react"
+
+
+export default function BudgetForm() {
+ 
+    const [budget, setBudget] = useState(0);
+    
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setBudget((+e.target.value));
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
+    return (
+    <form className="space-y-5">
+        <div className="flex flex-col space-y-5">
+            <label htmlFor="budget" className="text-4xl
+            text-blue-600 font-bold text-center"></label>
+            <input 
+            type="number"
+            id="budget"
+            placeholder="Define tu presupuesto"
+            className="w-full p-2 bg-white
+            border border-gray-200  rounded-lg text-2xl text-gray-700 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            value={budget}
+            onChange={handleChange}
+            min={0}
+            step={0.01}
+            required
+        
+        />
+        </div>
+        
+        <input 
+        type="submit"
+        value='Definir Presupuesto'
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer
+        w-full p-2 text-white font-black uppercase rounded-lg
+        disabled:opacity-48" 
+        disabled={isValid}
+        
+        />
+    </form>
+  )
+}
