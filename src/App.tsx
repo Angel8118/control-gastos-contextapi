7aa26d3677c4e355bddff5b86370fa5ba707eb4f
Angel8118 <angel8118campos@gmail.com>
2025-06-08 21:08:36 -0600

    Working with the component ExpenseDetail

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm";
 import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
 import ExpenseModal from "./components/ExpenseModal";
+import ExpenseList from "./components/ExpenseList";
 
 export default function App() {
 
@@ -24,7 +25,7 @@ export default function App() {
 
       {isValidBudget && (
         <main className="max-w-3xl mx-auto py-10">
-          
+          <ExpenseList />
           <ExpenseModal />
         </main>
       )}
